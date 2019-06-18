643554d431b0514f1968c78dd34418ba8ae1b9b2
cbertevello <lumzuc@bol.com.br>
2019-06-18 04:29:16 -0300

    Administrar & Criar Conta

--- receitas/admin.py
+++ receitas/admin.py
@@ -1,8 +1,12 @@
 from django.contrib import admin
 #from django.contrib.auth.models import Group
 #from .models import Snippet
+
+
 from .models import Prescricao, Remedio
 
+admin.site.site_header = 'Página de Administração do RepoMed'
 #admin.site.unregister(Group)
+
 admin.site.register(Prescricao)
 admin.site.register(Remedio)
--- receitas/views.py
+++ receitas/views.py
@@ -1,6 +1,12 @@
-from django.views import generic
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
+
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login
+
+from django.views import generic
+from django.views.generic import View
 from .models import Prescricao
+from .forms import UserForm
 
 class IndexView(generic.ListView):
     template_name = 'receitas/index.html'
@@ -16,3 +22,39 @@ class DetailView(generic.DetailView):
 class AlbumCreate(CreateView):
     model = Prescricao
     fields = ['nome_medico', 'data_ano_mes_dia', 'data_mes_texto', 'texto_receita']
+
+
+class UserFormView(View):
+    form_class = UserForm
+    template_name = 'receitas/registration_form.html'
+    # mostra um formulário "em branco"
+    def get(self, request):
+        form = self.form_class(None)
+        return render(request, self.template_name, {'form': form})
+
+    # processando dados do formulário
+    def post(self, request):
+        form = self.form_class(request.POST)
+
+        if form.is_valid():
+
+            user = form.save(commit=False)
+
+            # dados limpos (normalizados)
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password']
+            user.set_password(password)
+            user.save()
+
+            #retorna objetos "User" (se os dados estiverem corretos)
+            user = authenticate(username=username, password=password)
+
+            if user is not None:
+
+                if user.is_active:
+                    login(request, user)
+                    return redirect('receitas:index')
+
+        return render(request, self.template_name, {'form': form})
+
+
